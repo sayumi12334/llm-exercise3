f7e0350cbaa6a1ca8ddf04c6d6840a956f249cc2
Sayumi <buddikod@gmail.com>
2026-02-13 12:04:08 +1000

    createed DB to store cancer PDFS

---
+++ build_cancer_db.py
@@ -0,0 +1,49 @@
+# %%
+#Build chromaDB to store Cancer PDFs
+
+"""
+Docstring for llm-exercise3-dev.llm-exercise3.build_cancer_db
+"""
+
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_ollama import OllamaEmbeddings
+
+
+
+DOCS_DIR = "cancer_docs"
+PERSIST_DIR = "chroma_store"
+COLLECTION_NAME = "Cancer"
+
+EMBED_MODEL = "nomic-embed-text"
+CHAT_MODEL = "llama3.2:3b"
+
+
+# %% 2) Load ALL PDFs in the folder
+
+loader = DirectoryLoader(DOCS_DIR, glob="**/*.pdf", loader_cls=PyPDFLoader)
+documents = loader.load()
+
+
+# 2) Split into chunks
+splitter = RecursiveCharacterTextSplitter(chunk_size=3000, chunk_overlap=200)
+chunks = splitter.split_documents(documents)
+
+
+emb = OllamaEmbeddings(model="nomic-embed-text")
+
+vectordb = Chroma.from_documents(
+    documents=chunks,
+    embedding=emb,
+    persist_directory=PERSIST_DIR,
+    collection_name=COLLECTION_NAME,
+)
+vectordb.persist()
+print("✅ Saved to Chroma collection:", COLLECTION_NAME)
+
+
+        
+
+
+
---
+++ query_cancer.py
@@ -0,0 +1,46 @@
+# %%
+import os
+from langchain_community.vectorstores import Chroma
+from langchain_ollama import OllamaEmbeddings
+
+DOCS_DIR = "cancer_docs"
+PERSIST_DIR = "chroma_store"
+COLLECTION_NAME = "Cancer"
+emb = OllamaEmbeddings(model="nomic-embed-text")
+
+# %%
+"""
+import os
+from langchain_community.vectorstores import Chroma
+from langchain_ollama import OllamaEmbeddings
+
+DOCS_DIR = "cancer_docs"
+PERSIST_DIR = "chroma_store"
+COLLECTION_NAME = "Cancer"
+emb = OllamaEmbeddings(model="nomic-embed-text")
+
+vectordb = Chroma(
+    embedding_function=emb,
+    persist_directory=PERSIST_DIR,
+    collection_name=COLLECTION_NAME,
+)
+
+questions = [
+    "What are the main screening methods for lung cancer?",
+    "How is mammography used in early cancer detection?",
+    "What role does AI or machine learning play in cancer imaging diagnosis?",
+    "Describe common biomarkers used for cancer diagnosis.",
+    "What are the WHO guidelines for early diagnosis of cervical cancer?",
+    "Compare different methods for detecting lung cancer early.",
+    "Explain liquid biopsy in cancer diagnosis",
+]
+
+for q in questions:
+    print("\n")
+    print("Q:", q)
+    results = vectordb.similarity_search(q, k=2)
+    for i, doc in enumerate(results, 1):
+        print(doc.page_content[:500])
+
+# %%
+"""
